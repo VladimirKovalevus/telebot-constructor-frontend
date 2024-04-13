611bd5dcc88c8391878b707efdc97872714b6805
Aleksey Popov <alekseypopov.dev@gmail.com>
2024-04-13 16:29:50 +0700

    feat: update imports for nodes

--- src/pages/Bot/api/chatbot/nodes/index.ts
+++ src/pages/Bot/api/chatbot/nodes/index.ts
@@ -1,5 +1,7 @@
+export { DebugChat } from "./debug-chat";
 export { MatchMessage } from "./match-message";
 export { Message } from "./message";
+export { OnCommand } from "./on-command";
 export { OnMessage } from "./on-message";
 export { SendMessage } from "./send-message";
-export { DebugChat } from "./debug-chat";
+export { Timer } from "./timer";
